b37a7be8b2aedcc6d65958102568efba02de6146
mgnedash <gnedasch2011@yandex.ru>
2020-08-21 12:53:19 +0300

    init

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -19,6 +19,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="image/x-icon" href="/images/main/favicon/favicon.ico" rel="icon">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
